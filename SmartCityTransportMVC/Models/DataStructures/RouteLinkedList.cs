041f887679f591a9d3ec1126e01d0007f2f41faf
Duygu <148623218+duygusezr@users.noreply.github.com>
2025-05-10 11:11:55 +0300

    RouteLinkedList

---
+++ SmartCityTransportMVC/Models/DataStructures/RouteLinkedList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartCityTransportMVC.Models.DataStructures
+{
+    public class RouteLinkedList
+    {
+        public List<string> OptimizeRoute(List<string> route, List<double> trafficData)
+        {
+            return route.Zip(trafficData, (stop, traffic) => (stop, traffic))
+                        .Where(x => x.traffic < 0.7)
+                        .Select(x => x.stop)
+                        .ToList();
+        }
+    }
+}
